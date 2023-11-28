6bcbbb9bd210ff7bed0d755c38fb5cf5dd2d5278
Binyam <biatsb5656@gmail.com>
2023-11-28 15:34:47 -0800

    write a function in c inserts a number again

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,9 +1,9 @@
 #include "lists.h"
 
- /**
+/**
  * insert_node - inserts a new node
  * at a given position.
- * @head: head of a lists.
+ * @head: head of a list.
  * @number: index of the list where the new node is
  * added.
  * Return: the address of the new node, or NULL if it
