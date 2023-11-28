3b5f2434fb3033ceb8b6e209656654f557ab2133
Binyam <biatsb5656@gmail.com>
2023-11-28 14:43:24 -0800

    make change on the code

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for i in range(10):
-    for x in range(10):
-        if(i == 8 and x == 9):
+    for j in range(10):
+        if(i == 8 and j == 9):
             print("{}".format(str(i) + str(j)))
-        elif(x > i):
+        elif(j > i):
             print("{}".format(str(i) + str(j)) + ", ", end="")
