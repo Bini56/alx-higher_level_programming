7f15b542e5ae8e73f3ecfd914147d88c5baf475c
Binyam <biatsb5656@gmail.com>
2023-12-01 23:11:32 -0800

    update the program in some spellings

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     if idx < 0:
-        print none
-    elif idx > len(my_list):
-        return none
+        return None
+    elif idx >= len(my_list):
+        return None
     else:
         return my_list[idx]
-        
