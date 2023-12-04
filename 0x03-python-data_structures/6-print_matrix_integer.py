17e1d951fcd086a952c29c4e4fbe6d080eed50a0
Binyam <biatsb5656@gmail.com>
2023-12-04 11:28:36 -0800

    printing matrix of intigers

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for x in matrix:
-        for number in x:
+    for element in matrix:
+        for number in element:
             print("{:d}".format(number), end="")
-            if(number is not x[len(x)-1]):
+            if(number is not element[len(element)-1]):
                 print(" ", end="")
         print()
