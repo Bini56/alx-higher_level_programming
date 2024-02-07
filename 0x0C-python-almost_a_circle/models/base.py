63e68c8eef56fbcd2d9a23719985fc0dd6a585c2
Binyam <biatsb5656@gmail.com>
2024-02-07 00:22:22 -0800

    I writted my first class

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,19 @@
+#!user/bin/python3
+"""0-main"""
+from model.base import base
+
+if __name__ = "__main__":
+    b1 = base()
+    print(b1.id)
+
+    b2 = base()
+    print(b2.id)
+
+    b3 = base()
+    print(b3.id)
+
+    b4 = base()
+    print(b4.id)
+
+    b5 = base()
+    print(b5.id)
