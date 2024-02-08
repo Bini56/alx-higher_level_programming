ec9fda710c7f50942e955f70d597be99e1429ce3
Binyam <biatsb5656@gmail.com>
2024-02-08 00:13:41 -0800

    modify the class atr in rectangle

--- 0x0C-python-almost_a_circle/rectangle.py
+++ 0x0C-python-almost_a_circle/rectangle.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
+"""
+class called  rectangle
+"""
 
-""" this is rectangle class inhereted from base class"""
 
 from models.base import Base
 
-class rectangle(Base):
-      """this Represents a rectangle and inherits som attrs from base"""
+
+class Rectangle(Base):
+    """this represents a rectangle and iinherits som attrs from base"""
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """test"""
@@ -14,4 +17,8 @@ class rectangle(Base):
         self.x = x
         self.y = y
         super().__init__(id)
-     
+
+    @property
+    def width(self):
+        """width getter"""
+        return self.__width
