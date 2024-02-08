18e454d1220ddbee16c24212c2983e4f932644f0
Binyam <biatsb5656@gmail.com>
2024-02-08 01:10:31 -0800

    assign key/value argument to attribitr

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,21 +1,32 @@
 #!/usr/bin/python3
-""" this  contains class Square implements class Rectangle"""
+"""
+    contains class Square implements class Rectangle
+"""
 from models.rectangle import Rectangle
 
+
 class Square(Rectangle):
-     """Square implements rectangle"""
-     def __init__(self, size, x=0, y=0, id=none):
-         """  initialises Square (overrides Rectangle init)"""
-         super()__init(size, size, x, y, id)
+    """
+        Square implements rectangle
+    """
+    def __init__(self, size, x=0, y=0, id=None):
+        """
+            initialises Square (overrides Rectangle init)
+        """
+        super().__init__(size, size, x, y, id)
 
-@property
-def size(self):
-    """this returns the size of the square"""
-    return self.width
+    @property
+    def size(self):
+        """
+            returns the size of the square
+        """
+        return self.width
 
-   @size.setter
+    @size.setter
     def size(self, value):
-        """sets the value of size"""
+        """
+            sets the value of size
+        """
         if type(value) != int:
             raise TypeError("width must be an integer")
         if value <= 0:
@@ -23,10 +34,24 @@ def size(self):
 
         self.width = value
         self.height = value
-            def update(self, *args, **kwargs):
+
+    def update(self, *args, **kwargs):
         """
             assigns key/value argument to attributes
             kwargs is skipped if args is not empty
             Args:
                 *args -  variable number of no-keyword args
                 **kwargs - variable number of keyworded args
+        """
+        if len(args) == 0:
+            for key, val in kwargs.items():
+                self.__setattr__(key, val)
+            return
+
+        try:
+            self.id = args[0]
+            self.size = args[1]
+            self.x = args[2]
+            self.y = args[3]
+        except IndexError:
+            pass
